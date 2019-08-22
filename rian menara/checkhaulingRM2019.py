7572249a77408c1d7b5ee079a73f745ddd4352e6
Dhita Irma <dhita.irma@gmail.com>
2019-08-23 04:25:57 +0800

    Separate hauling todok and rian menara

---
+++ rian menara/checkhaulingRM2019.py
@@ -0,0 +1,23 @@
+#! python3
+# check_hauling.py
+
+import openpyxl as xl
+wb1 = xl.load_workbook('Hauling RM 2019.xlsx')
+haulingRM = wb1.active
+
+wb2 = xl.load_workbook('dataBB_RM.xlsx')
+dataBB = wb2.active
+
+print('Reading rows...')
+for row1 in range(3, haulingRM.max_row + 1):
+    voucher1 = haulingRM.cell(row=row1, column=2).value
+    haulingRM.cell(row1, 1).value = 'PENDING'
+    for row2 in range(7, dataBB.max_row + 1):
+        voucher2 = int(str(dataBB.cell(row2, 4).value)[:5])
+        if voucher2 == voucher1:
+            for col in range(1, dataBB.max_column + 1):
+                haulingRM.cell(row=row1, column=col + 2).value = dataBB.cell(row=row2, column=col).value
+                haulingRM.cell(row=row1, column=1).value = 'OK'
+
+print('Saving workbook...')
+wb1.save('Hauling RM 2019 update.xlsx')
---
+++ todok/check_hauling_todok.py
@@ -0,0 +1,30 @@
+import openpyxl as xl
+from openpyxl.styles import PatternFill
+from openpyxl.styles.colors import WHITE
+from openpyxl.styles.colors import YELLOW
+from openpyxl.styles.colors import RED
+
+
+wb = xl.load_workbook('dataBB_todok.xlsx')
+dataBB = wb.active
+
+redFill = PatternFill(start_color=RED, end_color=RED, fill_type='solid')
+whiteFill = PatternFill(start_color=WHITE, end_color=WHITE, fill_type='solid')
+yellowFill = PatternFill(start_color=YELLOW, end_color=YELLOW, fill_type='solid')
+
+print('Reading rows...')
+for rowBB in range(7, dataBB.max_row + 1):
+    current_voucherBB = int(dataBB.cell(rowBB, 4).value[:5])
+    if dataBB.cell(rowBB+1, 4).value is not None:
+        next_voucherBB = int(str(dataBB.cell(rowBB+1, 4).value)[:5])
+        if next_voucherBB - current_voucherBB == 0:
+            dataBB.cell(rowBB, 4).fill = redFill
+            dataBB.cell(rowBB+1, 4).fill = redFill
+        elif next_voucherBB - current_voucherBB > 1:
+            dataBB.cell(rowBB, 4).fill = yellowFill
+        else:
+            dataBB.cell(rowBB, 4).fill = whiteFill
+
+
+print('Saving workbook...')
+wb.save('dataBB_todok_checked.xlsx')
